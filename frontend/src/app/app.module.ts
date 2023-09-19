ba6bd6a55d4c51b53fff16ae7dd3cb20359a52a3
fabanhas <fabinho.alves91@gmail.com>
2023-09-19 20:36:44 -0300

    feat: basic components for list and schedule transactions

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { ScheduleTransactionComponent } from './schedule-transaction/schedule-transaction.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent,
+    ScheduleTransactionComponent,
+    ScheduleComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
