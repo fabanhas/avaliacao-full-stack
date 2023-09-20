aab5325ef6eeecd9469c56cad07b13f6b346b7c6
fabanhas <fabinho.alves91@gmail.com>
2023-09-20 01:55:03 -0300

    feat: validate fields and show error message for user

--- frontend/src/app/schedule/schedule.component.ts
+++ frontend/src/app/schedule/schedule.component.ts
@@ -7,7 +7,7 @@ import { Transaction } from '../model/Transaction';
   templateUrl: './schedule.component.html'
 })
 export class ScheduleComponent {
-  transaction: Transaction = new Transaction(); // Instância vazia para o formulário
+  transaction: Transaction = new Transaction();
   transactions: Transaction[] = [];
 
   constructor(private transactionService: TransactionService) { }
@@ -16,22 +16,79 @@ export class ScheduleComponent {
     this.getAllTransactions();
   }
 
+  formErrors = {
+    fromAccount: '',
+    toAccount: '',
+    value: '',
+    transferDate: '',
+    operation: ''
+  };
+
+  formSubmitted = false;
+
+  validateFormFields(): boolean {
+    let isValid = true;
+
+    if (!this.transaction.fromAccount) {
+      this.formErrors.fromAccount = 'A conta de origem é obrigatória.';
+      isValid = false;
+    } else {
+      this.formErrors.fromAccount = '';
+    }
+
+    if (!this.transaction.toAccount) {
+      this.formErrors.toAccount = 'A conta de destino é obrigatória.';
+      isValid = false;
+    } else {
+      this.formErrors.toAccount = '';
+    }
+
+    if (this.transaction.value === null || isNaN(Number(this.transaction.value))) {
+      this.formErrors.value = 'O valor é obrigatório e deve ser um número.';
+      isValid = false;
+    } else {
+      this.formErrors.value = '';
+    }
+
+    if (!this.transaction.transferDate) {
+      this.formErrors.transferDate = 'A data de transferência é obrigatória.';
+      isValid = false;
+    } else {
+      this.formErrors.transferDate = '';
+    }
+
+    if (!this.transaction.operation) {
+      this.formErrors.operation = 'A operação é obrigatória.';
+      isValid = false;
+    } else {
+      this.formErrors.operation = '';
+    }
+
+    return isValid;
+  }
+
   getAllTransactions(): void{
     this.transactionService.getAllTransactions()
       .subscribe(data => {
-        this.transactions = data; // Armazena os dados das transações no array transactions
+        this.transactions = data;
       });
   }
   
   scheduleTransaction(): void {
-    this.transactionService.scheduleTransaction(this.transaction).subscribe(
-      (response) => {
-        console.log('Transação agendada com sucesso:', response);
-        this.getAllTransactions();
-      },
-      (error) => {
-        console.error('Erro ao agendar transação:', error);
-      }
-    );
+    this.formSubmitted = true;
+
+    if (this.validateFormFields()) {
+      this.transactionService.scheduleTransaction(this.transaction).subscribe(
+        (response) => {
+          console.log('Transação agendada com sucesso:', response);
+          this.getAllTransactions();
+        },
+        (error) => {
+          console.error('Erro ao agendar transação:', error);
+        }
+      );
+    }
   }
+
+
 }
